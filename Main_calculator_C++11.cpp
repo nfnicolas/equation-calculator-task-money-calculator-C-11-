ae72f61d963218b8a82cf0e1f40f95678d172766
nfnicolas <nicolas198320@yahoo.com>
2016-09-07 01:30:52 +0300

    Add files via upload

---
+++ Main_calculator_C++11.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<type_traits>
+using namespace std;
+template<typename T>
+struct Number {
+	T num;
+	void set() { cin >> num; }
+};
+template<typename T,typename Y>
+Number<typename common_type<T,Y>::type>
+operator+(const Number<T>&lhs, const Number<Y>&rhs) 
+{ 
+	return{ lhs.num + rhs.num };
+}
+template<typename T,typename Y>
+Number<typename common_type<T,Y>::type>
+operator-(const Number<T>&lhs, const Number<Y>&rhs)
+{
+	return{ lhs.num - rhs.num };
+}
+int main(int argc, char*argv[])
+{
+	cout << "This calculator perfoms arithmetic operations such: ' + ' ' - ' ' / ' '*'\n";
+	cout << "enter two numbers please\n";
+	Number<int>n{ 0 };
+	Number<double>m{ 0 };
+	n.set();
+	m.set();
+	cout << "enter action\n"; 
+
+	//......
+
+	cout << (n + m).num;
+	cout << (n - m).num;
+	return 0;
+}
